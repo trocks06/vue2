65288c917edb5c4d48bf38beaff63adb0dad71e9
trocks06 <denishatkov17@gmail.com>
2025-03-19 11:57:38 +0700

    Фикс методов перемещения карточек и улучшение кода

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 Vue.component('to-do-list', {
     props: {
-        column1: {
-            type: Array,
-            required: true,
-        },
-        column2: {
-            type: Array,
-            required: true,
-        },
-        column3: {
-            type: Array,
-            required: true,
-        }
+        column1: { type: Array, required: true },
+        column2: { type: Array, required: true },
+        column3: { type: Array, required: true }
     },
     template: `
     <div class="main-div">
@@ -19,9 +10,10 @@ Vue.component('to-do-list', {
             <div v-for="(card, index) in column1" :key="index" class="content">
                 <h3>{{ card.name }}</h3>
                 <ul class="no-marker">
-                    <li v-for="(task, index) in card.tasks" :key="index">
-                        <input type="checkbox" :checked="task.taskCompleted"
-                        @change="toggleTaskCompletion(task, index)">
+                    <li v-for="(task, taskIndex) in card.tasks" :key="taskIndex">
+                        <input type="checkbox" 
+                               :checked="task.taskCompleted" :disabled="column2.length >= 8"
+                               @change="toggleTaskCompletion(card, task, 'column1', index)">
                         {{ task.taskName }}
                     </li>
                 </ul>
@@ -31,9 +23,10 @@ Vue.component('to-do-list', {
             <div v-for="(card, index) in column2" :key="index" class="content">
                 <h3>{{ card.name }}</h3>
                 <ul class="no-marker">
-                    <li v-for="(task, index) in card.tasks" :key="index">
-                        <input type="checkbox" :checked="task.taskCompleted"
-                        @change="toggleTaskCompletion(task, index)">
+                    <li v-for="(task, taskIndex) in card.tasks" :key="taskIndex">
+                        <input type="checkbox" 
+                               :checked="task.taskCompleted"
+                               @change="toggleTaskCompletion(card, task, 'column2', index)">
                         {{ task.taskName }}
                     </li>
                 </ul>
@@ -43,87 +36,56 @@ Vue.component('to-do-list', {
             <div v-for="(card, index) in column3" :key="index" class="content">
                 <h3>{{ card.name }}</h3>
                 <ul class="no-marker">
-                    <li v-for="(task, index) in card.tasks" :key="index">
-                        <input type="checkbox" :checked="task.taskCompleted"
-                        @change="toggleTaskCompletion(task)">
+                    <li v-for="(task, taskIndex) in card.tasks" :key="taskIndex">
+                        <input type="checkbox" 
+                               :checked="task.taskCompleted"
+                               @change="toggleTaskCompletion(card, task, 'column3', index)">
                         {{ task.taskName }}
                     </li>
                 </ul>
+                <p>Дата выполнения: {{ card.completedDate }}</p>
             </div>
         </div>
     </div>
     `,
-    data() {
-        return {};
-    },
     methods: {
-        toggleTaskCompletion(task) {
+        toggleTaskCompletion(card, task, column, index) {
             task.taskCompleted = !task.taskCompleted;
-            this.updateLocalStorage()
+            this.checkTaskComplete(card, index, column);
+            this.updateLocalStorage();
         },
         checkTaskComplete(card, index, column) {
-            let tasks = card.tasks.length
-            let completedTasks = card.tasks.filter(task => task.taskCompleted).length;
-            if (column === 'column1') {
-                if (completedTasks >= tasks * 0.5) {
-                    this.$emit('move-to-column2', card, index);
-                }
+            const tasks = card.tasks.length;
+            const completedTasks = card.tasks.filter(task => task.taskCompleted).length;
+
+            if (column === 'column1' && completedTasks >= tasks * 0.5) {
+                this.$emit('move-to-column2', card, index, column);
             } else if (column === 'column2') {
                 if (completedTasks < tasks * 0.5) {
                     this.$emit('move-to-column1', card, index);
                 } else if (completedTasks === tasks) {
                     this.$emit('move-to-column3', card, index);
+                    card.completedDate = new Date();
+                    card.completedDate = card.completedDate.toLocaleString()
                 }
-            } else if (column === 'column3') {
-                if (completedTasks >= tasks * 0.5) {
-                    this.$emit('move-to-column2', card, index);
-                }
+            } else if (column === 'column3' && completedTasks != tasks) {
+                this.$emit('move-to-column2', card, index, column);
             }
-            this.updateLocalStorage()
         },
         updateLocalStorage() {
             localStorage.setItem('column1', JSON.stringify(this.column1));
             localStorage.setItem('column2', JSON.stringify(this.column2));
             localStorage.setItem('column3', JSON.stringify(this.column3));
         }
-    },
-    watch: {
-        column1(newVal) {
-            newVal.forEach((card, index) => {
-                this.checkTaskComplete(card, index, 'column1');
-            });
-        },
-        column2(newVal) {
-            newVal.forEach((card, index) => {
-                this.checkTaskComplete(card, index, 'column2');
-            });
-        },
-        column3(newVal) {
-            newVal.forEach((card, index) => {
-                this.checkTaskComplete(card, index, 'column3');
-            });
-        },
     }
-})
+});
 
 Vue.component('card-create', {
     props: {
-        isModalOpen: {
-            type: Boolean,
-            required: true
-        },
-        column1: {
-            type: Array,
-            required: true,
-        },
-        column2: {
-            type: Array,
-            required: true,
-        },
-        column3: {
-            type: Array,
-            required: true,
-        }
+        isModalOpen: { type: Boolean, required: true },
+        column1: { type: Array, required: true },
+        column2: { type: Array, required: true },
+        column3: { type: Array, required: true }
     },
     template: `
     <div v-show="isModalOpen" class="modal">
@@ -135,12 +97,15 @@ Vue.component('card-create', {
             <input type="text" placeholder="Название карточки" v-model="name">
             <h5>Задания</h5>
             <button type="button" @click="addTask">Добавить задание</button>
-            <div v-for="(input, index) in inputs" :key='index' id="tasker">
-                <input type="text" placeholder="Задание" v-model="newTasks[index]">
+            <div v-for="(input, index) in inputs" :key="index" id="tasker">
+                <input type="text" 
+                       placeholder="Задание" 
+                       v-model="newTasks[index]"
+                       v-if="index < inputs">
             </div>
             <div>
                 <input type="submit" value="Создать">
-                <button type="button" @click="closeModal"">Закрыть</button>
+                <button type="button" @click="closeModal">Закрыть</button>
             </div>
         </form>
     </div>
@@ -150,65 +115,55 @@ Vue.component('card-create', {
             name: '',
             newTasks: [],
             errors: [],
-            inputs: 3,
+            inputs: 3
         }
     },
     methods: {
         saveCard() {
-            this.errors = []
-            this.name = this.name.trim()
-            for (let i = 0; i < this.newTasks.length; i++) {
-                this.newTasks[i] = this.newTasks[i].trim()
-                if (this.newTasks[i] === '') {
-                    this.newTasks.splice(i, 1)
-                    i--;
-                }
-            }
-            if (!this.name || this.name === '') {
-                this.errors.push("Введите название карточки")
+            this.errors = [];
+            this.name = this.name.trim();
+            this.newTasks = this.newTasks.filter(task => task && task.trim() !== '');
+
+            if (!this.name) {
+                this.errors.push("Введите название карточки");
             }
             if (this.newTasks.length < 3 || this.newTasks.length > 5) {
-                this.errors.push("Добавьте от трех до пяти заданий")
+                this.errors.push("Добавьте от трех до пяти заданий");
             }
-            if (this.name && this.newTasks.length <= 5 && this.newTasks.length >= 3) {
+            if (this.name && this.newTasks.length >= 3 && this.newTasks.length <= 5) {
                 let card = {
                     name: this.name,
-                    tasks: [],
-                }
-                for (let i = 0; i < this.newTasks.length; i++) {
-                    let task = {
-                        taskName: this.newTasks[i],
-                        taskCompleted: false,
-                    };
-                    card.tasks.push(task)
-                }
+                    tasks: this.newTasks.map(task => ({
+                        taskName: task,
+                        taskCompleted: false
+                    })),
+                    completedDate: null
+                };
                 this.$emit('add-card', card);
                 this.closeModal();
-                this.name = null
-                this.newTasks = []
+                this.name = '';
+                this.newTasks = [];
+                this.inputs = 3;
             }
         },
         addTask() {
-            this.errors = []
-            if(this.inputs === 5) {
-                this.errors.push("Больше пяти заданий сделать нельзя")
+            this.errors = [];
+            if (this.inputs >= 5) {
+                this.errors.push("Больше пяти заданий сделать нельзя");
             } else {
-                this.inputs++
+                this.inputs++;
+                this.newTasks.push('');
             }
         },
         closeModal() {
             this.$emit('modal-close');
-        },
+            this.name = '';
+            this.newTasks = [];
+            this.inputs = 3;
+            this.errors = [];
+        }
     }
-})
-
-Vue.component('column-block', {
-    template: `
-    <div class="block">
-        
-    </div>
-    `
-})
+});
 
 let app = new Vue({
     el: '#app',
@@ -234,9 +189,14 @@ let app = new Vue({
             this.column1.push(card);
             this.saveColumnData()
         },
-        moveToColumn2(card, index) {
-            this.column1.splice(index, 1)
-            this.column2.push(card);
+        moveToColumn2(card, index, column) {
+            if (column === 'column1') {
+                this.column1.splice(index, 1)
+                this.column2.push(card);
+            } else if (column === 'column3') {
+                this.column3.splice(index, 1)
+                this.column2.push(card);
+            }
             this.saveColumnData()
         },
         moveToColumn3(card, index) {
